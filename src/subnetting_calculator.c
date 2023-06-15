dc19245b8a47c9a45f39ea4aedb87d668f2a9d38
Letder40 <adrian04rv@gmail.com>
2023-06-16 00:31:07 +0200

    addition of support for arguments

--- src/subnetting_calculator.c
+++ src/subnetting_calculator.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 void print_network_range(int decimal_network_identifier[4], int decimal_broadcast_identifier[4]){
     for (int i=0; i<4; i++){
@@ -80,16 +81,18 @@ int* decimal_of_ip(int bytes[4][8]){
     return digits;
 }
 
+struct Octet {
+    int digits[4];
+    int counter;
+};
+
 int** get_ip(){
     int digit;
     int octet_index = 0;
 
     // octects declaration
 
-    struct Octet {
-        int digits[4];
-        int counter;
-    };
+   
 
     struct Octet octets[4];
     struct Octet *octet_ptr;
@@ -120,7 +123,7 @@ int** get_ip(){
         digit = digit - '0';
 
         if (digit < 0 || digit > 9) {
-            printf("[!] invalid IP address -> not a valid digit\n");
+            printf("[!] invalid address -> not a valid digit\n");
             exit(1);
         }
         
@@ -128,7 +131,7 @@ int** get_ip(){
         octet_ptr->counter++;
 
         if (octet_ptr->counter > 3) {
-            printf("[!] invalid IP address -> greeter value than 255\n");
+            printf("[!] invalid address -> greeter value than 255\n");
             exit(1);
         }
     }
@@ -146,7 +149,7 @@ int** get_ip(){
         }
 
         if (digit_octs[i] > 255 || digit_octs[i] < 0) {
-            printf("[!] invalid IP address -> a not valid value has been provided in the ip address\n");
+            printf("[!] invalid address -> a not valid value has been provided in the ip address\n");
             exit(1);
         }
         
@@ -155,11 +158,11 @@ int** get_ip(){
     // multidim array.
 
     // external layer allocation
-    int **ip_bits = malloc(sizeof(int*) * 4);
+    int **address_bits = malloc(sizeof(int*) * 4);
 
     // internal layer allocation
     for (int i = 0; i<4; i++){
-        ip_bits[i] = malloc(sizeof(int) * 8);
+        address_bits[i] = malloc(sizeof(int) * 8);
     }
 
 
@@ -169,23 +172,124 @@ int** get_ip(){
         int to_convert = digit_octs[byte_i];
         for (int bit_i = 7; bit_i >= 0; bit_i--) {
             int bit = to_convert % 2;
-            ip_bits[byte_i][bit_i] = bit;
+            address_bits[byte_i][bit_i] = bit;
             to_convert /= 2;
         }
     }
 
-    return ip_bits;
+    return address_bits;
 }
 
-int main() {
 
-    printf("[?] ip address -> ");
-    int** ip_bits = get_ip();
-    printf("[?] netmask -> ");
-    int** netmask_bits = get_ip();
+int** address_to_bits(char* arg){
+    #define TRUE 1
+    
+    struct Octet octets[4];
+    struct Octet *octet_ptr;
+    
+    for (int i=0; i<=3; i++){
+        octets[i].counter = 0;
+    }
+
+    int octect_index = 0;
+    int counter = 0;
+
+    while(TRUE){
+        octet_ptr = &octets[octect_index];
+
+        if (arg[counter] == '\0'){
+            break;
+        }
+
+        if (octet_ptr->counter > 3) {
+            printf("[!] invalid address -> greeter value than 255\n");
+            exit(1);
+        }
+
+        if (arg[counter] == '.'){
+            octect_index ++;
+            counter++;
+            continue;
+        }
+
+        int digit = arg[counter] - '0';
+        octet_ptr->digits[octet_ptr->counter] = digit;
+        octet_ptr->counter++;
+        counter++;
+
+    }
+
+    int digit_octs[4];
+
+    for (int i=0; i<4; i++){
+        if (octets[i].counter == 3){
+            digit_octs[i] = octets[i].digits[0] * 100 + octets[i].digits[1] * 10 + octets[i].digits[2];
+        }else if(octets[i].counter == 2){
+            digit_octs[i] = octets[i].digits[0] * 10 + octets[i].digits[1]; 
+        }else if(octets[i].counter == 1){
+            digit_octs[i] = octets[i].digits[0];
+        }
+
+        if (digit_octs[i] > 255 || digit_octs[i] < 0) {
+            printf("[!] invalid address -> a not valid value has been provided in the ip address\n");
+            exit(1);
+        }
+
+    }
+
+     // multidim array.
+
+    // external layer allocation
+    int **address_bits = malloc(sizeof(int*) * 4);
+
+    // internal layer allocation
+    for (int i = 0; i<4; i++){
+        address_bits[i] = malloc(sizeof(int) * 8);
+    }
+
+    for (int byte_i = 0; byte_i < 4; byte_i++) {
+        int to_convert = digit_octs[byte_i];
+        for (int bit_i = 7; bit_i >= 0; bit_i--) {
+            int bit = to_convert % 2;
+            address_bits[byte_i][bit_i] = bit;
+            to_convert /= 2;
+        }
+    }
+
+    return address_bits;
+
+}
+
+int main(int argv ,char** args) {
+    int** netmask_bits;
+    int** ip_bits;
+
+    if (argv == 1){
+        char* input = malloc(sizeof(char) * 15);
+        printf("[ IPv4 addresses ]\n");
+        
+        printf("[?] ip address            ");
+        fgets(input, 15, stdin);
+        input[strcspn(input, "\n")] = '\0';
+        ip_bits = address_to_bits(input);
+
+        printf("[?] network mask          ");
+        fgets(input, 15, stdin);
+        input[strcspn(input, "\n")] = '\0';
+        netmask_bits = address_to_bits(input);
+        free(input);
+
+    }else if (argv == 3) {
+        ip_bits = address_to_bits(args[1]);
+        netmask_bits = address_to_bits(args[2]);  
+        printf("[ IPv4 addresses ]\n");
+        printf("[#] ip address            %s\n", args[1]);
+        printf("[#] network mask          %s\n", args[2]);      
+    }else{
+        printf("Syntx error, syntax: <binary> ip_adress network_mask");
+    }
 
     // Checking if netmask is a valid one
-
     int bit_change = 1;
     int bit_change_counter = 0;
     for (int x=0; x<4; x++){
@@ -229,8 +333,8 @@ int main() {
 
     print_binary_representations_ptr("[#] ip address            ", ip_bits);
     print_binary_representations_ptr("[#] network mask          ", netmask_bits);
-    print_binary_representations("[#] network identifier      ", network_identifier);
-    print_binary_representations("[#] broadcast identifier     ", broadcast_identifier);
+    print_binary_representations("[#] network identifier    ", network_identifier);
+    print_binary_representations("[#] broadcast identifier  ", broadcast_identifier);
 
     // Getting decimal representation of network identifier, and broadcast identifier
 
@@ -241,7 +345,7 @@ int main() {
 
     printf("\n[ Decimal representation ] \n");
 
-    print_identifiers("[#] network identifier   ", decimal_network_identifier);
+    print_identifiers("[#] network identifier    ", decimal_network_identifier);
     print_identifiers("[#] broadcast identifier  ", decimal_broadcast_identifier);
 
     // printing network range
